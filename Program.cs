7f549e943c3af547c5bfb0ff0e2b76a3916d5935
Joker-smile <1020733278@qq.com>
2026-04-22 09:25:43 +0800

    增加版本号显示

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace StockTracker;
 
 class Program
 {
-    public const string APP_VERSION = "v1.1.2";
+    public const string APP_VERSION = "v1.1.5";
     private static Mutex? mutex = null;
 
     [STAThread]
